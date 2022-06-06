03b316d0155cb157af2e861ae10db2027dbe5efc
Rob Zmudzinski <rob@rob.com>
2022-06-06 14:49:53 -0700

    Towards implementing icon selection

--- MemoryMatrix/MemoryMatrixApp.swift
+++ MemoryMatrix/MemoryMatrixApp.swift
@@ -7,10 +7,34 @@
 
 import Foundation
 
+class IconsSet {
+	var name = ""
+	var path = ""
+	var icons = [String]()
+	init(name: String, path: String, icons: [String]) {
+		self.name = name
+		self.path = path
+		self.icons = icons
+	}
+}
+
 class MemoryMatrixApp {
 	static let shared = MemoryMatrixApp()
+	public private(set) var icons = [IconsSet]()
 	
 	private init() {
-		
+		if let resourcePath = Bundle.main.resourcePath {
+			let iconsPath = "\(resourcePath)/GameIcons/Icons"
+			let fileManager = FileManager()
+			if let iconsList = try? fileManager.contentsOfDirectory(atPath: iconsPath) {
+				for iconSet in iconsList {
+					print(iconSet)
+					let iconsSetPath = "\(resourcePath)/GameIcons/Icons/\(iconSet)"
+					if let iconSetIcons = try? fileManager.contentsOfDirectory(atPath: iconsSetPath) {
+						icons.append(IconsSet(name: iconSet, path: iconsSetPath, icons: iconSetIcons))
+					}
+				}
+			}
+		}
 	}
 }
--- MemoryMatrix/SelectIconsViewController.swift
+++ MemoryMatrix/SelectIconsViewController.swift
@@ -8,20 +8,109 @@
 import UIKit
 
 class SelectIconsTableCell: UITableViewCell {
+	private var iconInfo: IconsSet?
+	private var iconName = UILabel()
+	private var iconImage = UIImageView()
 	
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		
+		iconImage.sizeToFit()
+		iconImage.layer.borderWidth = 1
+		iconImage.layer.borderColor = UIColor.lightGray.cgColor
+		iconName.translatesAutoresizingMaskIntoConstraints = false
+		iconImage.translatesAutoresizingMaskIntoConstraints = false
+		
+		contentView.addSubview(iconName)
+		contentView.addSubview(iconImage)
+		
+		iconImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+		iconImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
+		iconImage.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.2).isActive = true
+		iconImage.heightAnchor.constraint(equalTo: iconImage.widthAnchor, multiplier: 1.0/1.0).isActive = true
+		
+		iconName.topAnchor.constraint(equalTo: iconImage.bottomAnchor, constant: 10).isActive = true
+		iconName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
+		iconName.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15).isActive = true
+		iconName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
+		
+	}
+	
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+	}
+	
+	func setIconInfo(iconInfo: IconsSet) {
+		self.iconInfo = iconInfo
+		let iconPath = "\(iconInfo.path)/\(iconInfo.icons[0])"
+		print(iconPath)
+		if let image = UIImage(contentsOfFile: iconPath) {
+			iconImage.image = image
+		}
+		iconName.text = iconInfo.name
+	}
+}
+
+struct FolderInfo {
+	var rootPath: String
+	var name: String
+	var icons: [String]
+}
+class IconsFolderTableCell: UITableViewCell {
+	private var folderInfo: FolderInfo?
+	private var folderName = UILabel()
+	
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		
+		contentView.addSubview(folderName)
+
+		folderName.translatesAutoresizingMaskIntoConstraints = false
+		folderName.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+		folderName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
+		
+		folderName.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10).isActive = true
+	}
+	
+	func setFolderInfo(folderInfo: FolderInfo) {
+		self.folderInfo = folderInfo
+		folderName.text = folderInfo.name.replacingOccurrences(of: "_", with: " ")
+	}
+	
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+	}
 }
 
 class SelectIconsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 	var tableView = UITableView()
+	var selected = ""
+	
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		10
+		MemoryMatrixApp.shared.icons.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = UITableViewCell()
-		cell.textLabel?.text = "\(indexPath.row)"
-		return cell
+		if let cell = tableView.dequeueReusableCell(withIdentifier: "icon-cell", for: indexPath) as? SelectIconsTableCell {
+			//cell.textLabel?.text = "\(MemoryMatrixApp.shared.icons[indexPath.row].name)"
+			cell.setIconInfo(iconInfo: MemoryMatrixApp.shared.icons[indexPath.row])
+			return cell
+		}
+		return UITableViewCell()
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		selected = MemoryMatrixApp.shared.icons[indexPath.row].name
+		if let cell = tableView.cellForRow(at: indexPath) {
+			cell.accessoryType = .checkmark
+		}
+	}
+	
+	func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+		if let cell = tableView.cellForRow(at: indexPath) {
+			cell.accessoryType = .none
+		}
 	}
 	
     override func viewDidLoad() {
@@ -34,6 +123,7 @@ class SelectIconsViewController: UIViewController, UITableViewDelegate, UITableV
 		tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
 		tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
 		
+		tableView.register(SelectIconsTableCell.self, forCellReuseIdentifier: "icon-cell")
 		tableView.delegate = self
 		tableView.dataSource = self
     }
