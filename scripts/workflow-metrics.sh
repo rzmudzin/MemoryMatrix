1fc316f3435ef43930c1001b440fe817b2a024a6
Rob Z <rob@phoenixroberts.com>
2023-02-01 16:06:04 -0700

    Update workflow-metrics.sh

--- scripts/workflow-metrics.sh
+++ scripts/workflow-metrics.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# owner="mobile"
+# repo="ios"
+# targetStep="Archivingproject"
+# targetWorkflow="BuildFeatureBranch"
+# authToken="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"
+# baseUrl="https://git.marriott.com/api/v3/repos"
+
 owner="rzmudzin"
 repo="memorymatrix"
 targetStep="Build"
 targetWorkflow="Archive"
 authToken="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"
+baseUrl="https://api.github.com/repos"
+
 outputFile="run-times.csv"
 jq="$HOME/jq/jq"
 
@@ -23,7 +32,7 @@ function installJQ() {
 
 function process_run() {
     #Process passed run id and its associated steps
-    jobsUrl="https://git.marriott.com/api/v3/repos/$owner/$repo/actions/runs/$1/jobs"
+    jobsUrl="$baseUrl/$owner/$repo/actions/runs/$1/jobs"
     echo "$jobsUrl"
     jobsJson=$(curl --location --request GET "$jobsUrl" \
 --header 'Accept: application/vnd.github+json' \
@@ -64,7 +73,7 @@ installJQ
 echo "workflow,runid,run_start,run_end,step,step_start,step_end" > ${outputFile}
 
 #Download all workflow runs for this repo
-runsUrl="https://git.marriott.com/api/v3/repos/$owner/$repo/actions/runs"
+runsUrl="$baseUrl/$owner/$repo/actions/runs"
 runsJson=$(curl --location --request GET "${runsUrl}" --header 'Accept: application/vnd.github+json' --header 'Authorization: Bearer '"${authToken}" --header 'X-GitHub-Api-Version: 2022-11-28')
 runs=($("${jq}" -r '.workflow_runs[] | .name, .status, .id' <<< "$runsJson" | tr -d '[]," '))
 
