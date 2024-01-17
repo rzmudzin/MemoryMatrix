c892fe805dd6e351c1e4a0d94946411fc5935e8d
rob <rob@rob.com>
2024-01-17 11:02:39 -0700

    Add size check to package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,1 +1,5 @@
 xcodebuild -exportArchive -archivePath ~/bld/MemoryMatrixApp.xcarchive -exportPath ~/ipa -exportOptionsPlist ~/export.plist
+
+find "~/bld/MemoryMatrixApp.xcarchive" -name "*.car" -print0 | xargs -0 -I {} python3 ./scripts/check-asset-size.py --cpath {} ${APP_SIZE_RESTRICTIONS}
+
+
