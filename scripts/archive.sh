840496cb9ba1a3814a3cccbe283283e76e455b01
Rob Z <rob@phoenixroberts.com>
2024-02-27 14:53:20 -0700

    Update archive.sh

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,2 +1,2 @@
 security find-identity -v -p codesigning
-xcodebuild -project MemoryMatrix.xcodeproj clean archive -scheme "MemoryMatrix" -configuration Release -archivePath ~/bld/MemoryMatrixApp.xcarchive CODE_SIGN_IDENTITY="$1" CODE_SIGN_STYLE=Manual PROVISIONING_PROFILE="cc7c62a4-7417-4578-b2f0-30b5f8fd50fe"
+xcodebuild -project MemoryMatrix.xcodeproj clean archive -scheme "MemoryMatrix" -configuration Release -archivePath ~/bld/MemoryMatrixApp.xcarchive CODE_SIGN_IDENTITY="$1" CODE_SIGN_STYLE=Manual PROVISIONING_PROFILE="$2"
