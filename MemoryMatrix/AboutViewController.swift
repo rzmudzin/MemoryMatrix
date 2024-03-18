bc6475a05efb773aaaff8f5d00f6e909eeb0b2bb
rob <rob@rob.com>
2024-03-17 20:27:15 -0700

    Wiring up description text

--- MemoryMatrix/AboutViewController.swift
+++ MemoryMatrix/AboutViewController.swift
@@ -13,7 +13,31 @@ class AboutViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        view.backgroundColor = .black
+        view.backgroundColor = .black
+        let label = UILabel()
+        label.backgroundColor = .black
+        label.textColor = .white
+        let aboutText = Bundle.main.object(forInfoDictionaryKey: "AboutText") as? String ?? "Some Hardcoded Stuff"
+        label.text = aboutText
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor),
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
+        
+//        NSLayoutConstraint(
+//            item: label,
+//            attribute: .centerY,
+//            relatedBy: .equal,
+//            toItem: view,
+//            attribute: .centerY,
+//            multiplier: 0.7,
+//            constant: 0
+//        ).isActive = true
     }
     
 }
